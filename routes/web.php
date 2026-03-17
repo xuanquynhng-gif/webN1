e223d1b00ff47515b8c000e3ee06fb1401a774d1
Litih-7705 <nguyethaquan@gmail.com>
2026-03-17 18:27:03 +0700

    Này 7.3 nha

--- routes/web.php
+++ routes/web.php
@@ -1,45 +1,45 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB; // Bắt buộc phải có dòng này để chạy Database
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
 Route::get('/', function () {
     return view ('welcome');
 });
 
-<<<<<<< HEAD
-
+Route::get('/luongmydao', function () {
+    return 'Lương Mỵ Đào';
+});
 
 Route::get("/xuanquynh","App\Http\Controllers\NameController@xuanquynh");
-=======
+
 Route::get("/qlsach/theloai","App\Http\Controllers\BookController@laythongtintheloai");
+
 Route::get("/qlsach/thongtinsach","App\Http\Controllers\BookController@laythongtinsach");
-<<<<<<< HEAD
+
 Route::get('/Kimloan', function () {
     return "Lam Thi Kim Loan";
-=======
+});
 
-<<<<<<< HEAD
 Route::get('/Thanh', function () {
     return "Chào bạn, đây là trang của Thanh";
-=======
+});
+
 Route::get('/thaolinh', function () {
     return 'Nguyễn Lê Thảo Linh';
-<<<<<<< HEAD
 });
->>>>>>> b45344d031197b0cea4677f0bf0bf4c7749bdf03
-=======
->>>>>>> b45344d031197b0cea4677f0bf0bf4c7749bdf03
->>>>>>> 365195415d9b9ed3bfed45088226985b46a3e302
+
+Route::get('/top-doanh-thu', function () {
+    $movies = DB::table('movie')
+                ->orderBy('budget', 'desc')
+                ->limit(10)
+                ->get();
+    
+    return view('top_budget', ['movies' => $movies]);
 });
->>>>>>> a1c8cbedd134402dedb9ce3a2ed93b1abf72ecbd
