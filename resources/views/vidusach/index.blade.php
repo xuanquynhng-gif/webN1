a2e9855079f111dd077ebe2c57ed673249f25c08
xuanquynhng-gif <030239230205@st.buh.edu.vn>
2026-03-27 13:55:22 +0700

    hoàn chỉnh

--- resources/views/vidusach/index.blade.php
+++ resources/views/vidusach/index.blade.php
@@ -2,15 +2,15 @@
 @section("title","Sách")
 @section("content")
 <div class='list-book'>
-@foreach($data as $row)
-<div class='book'>
-    <a href="{{url('sach/chitiet/'.$row->id)}}" style="text-decoration: none; color: black;">
-        <img src="{{asset('book_image/'.$row->file_anh_bia)}}" width='200px' height='200px'><br>
-        <b>{{$row->tieu_de}}</b>
-    </a>
-    <br/>
-    <i>{{number_format($row->gia_ban,0,",",".")}}đ</i>
-</div>
-@endforeach
+    @foreach($data as $row)
+        <div class='book'>
+            <a href="{{url('sach/chitiet/'.$row->id)}}" style="text-decoration: none; color: black;">
+                <img src="{{asset('book_image/'.$row->file_anh_bia)}}" width='200px' height='200px'><br>
+                <b>{{$row->tieu_de}}</b>
+            </a>
+            <br/>
+            <i>{{number_format($row->gia_ban,0,",",".")}}đ</i>
+        </div>
+    @endforeach
 </div>
 @endsection
