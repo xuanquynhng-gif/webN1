1754bc6f85520f70e1607f4e5df29f172a939d83
xuanquynhng-gif <030239230205@st.buh.edu.vn>
2026-03-30 20:59:40 +0700

    bài thực hành 2

--- routes/web.php
+++ routes/web.php
@@ -1,73 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\DB;
-use App\Http\Controllers\MovieController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
-    return view('welcome');});
-
-// Route của bạn Đào (Yêu cầu 3)
-Route::get('/luongmydao', function () {
-    return 'Lương Mỵ Đào';
+    return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('/sach','App\Http\Controllers\ViduLayoutController@sach');
-Route::get('/sach/theloai/{id}','App\Http\Controllers\ViduLayoutController@theloai');
-Route::get('/sach/chitiet/{id}', 'App\Http\Controllers\ViduLayoutController@chitiet');
-
-Route::get("/xuanquynh","App\Http\Controllers\NameController@xuanquynh");
-
-// Route của các thành viên khác trong nhóm
-Route::get("/xuanquynh", "App\Http\Controllers\NameController@xuanquynh");
-Route::get("/qlsach/theloai", "App\Http\Controllers\BookController@laythongtintheloai");
-Route::get("/qlsach/thongtinsach", "App\Http\Controllers\BookController@laythongtinsach");
-Route::get('/luongmydao', function () {
-    return 'Lương Mỵ Đào';
-});
-
-Route::get("/xuanquynh","App\Http\Controllers\NameController@xuanquynh");
-
-
-Route::get('/sach','App\Http\Controllers\ViduLayoutController@sach');
-
-Route::get("/xuanquynh","App\Http\Controllers\NameController@xuanquynh");
-
-Route::get("/qlsach/theloai","App\Http\Controllers\BookController@laythongtintheloai");
-Route::get("/qlsach/thongtinsach","App\Http\Controllers\BookController@laythongtinsach");
-
-Route::get('/Kimloan', function () {
-    return "Lam Thi Kim Loan";
-});
-
-Route::get('/Thanh', function () {
-    return "Chào bạn, đây là trang của Thanh";
-});
-
-
-Route::get('/thaolinh', function () {
-    return 'Nguyễn Lê Thảo Linh';
-});
-
-
-Route::get('/phimcanada', function () {
-    $data = DB::table('movie')
-                ->where('country_name', 'Canada')
-                ->get();
-
-    return view('phim_canada', ['ds_phim' => $data]);
-});
-
-Route::get("qlphim/theloaiphim", "App\Http\Controllers\MovieController@theloaiphim");
-
-Route::get('/top-10-phim', [MovieController::class, 'topMovies']);
-Route::get('/top-doanh-thu', function () {
-    $movies = DB::table('movie')
-                ->orderBy('budget', 'desc')
-                ->limit(10)
-                ->get();
-    
-    return view('top_budget', ['movies' => $movies]);
-});
-Route::get('/phim-thoi-luong-lon', [MovieController::class, 'longmovies']);
+require __DIR__.'/auth.php';
